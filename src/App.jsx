ea0c74bcfeff5b38167d9af2d4b8b3e3f14a8b85
Aadityx <aadityapatil784@gmail.com>
2025-12-22 18:03:32 +0530

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div style={{ paddingTop: '80px' }}>
+        <h1>Portfolio setup successful</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
+
