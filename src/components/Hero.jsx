a191d1e288542aa376d4925ff9e04c485df91b3c
Aadityx <aadityapatil784@gmail.com>
2025-12-22 18:08:13 +0530

    Hero added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import '../styles/hero.css';
+
+function Hero() {
+  return (
+    <section className="hero" id="hero">
+      <div className="hero-content">
+        <h1>Hi, I'm Aadityx</h1>
+        <p>Front-End Developer | Building Modern Web Experiences</p>
+        <a href="#projects" className="btn">See My Work</a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;
